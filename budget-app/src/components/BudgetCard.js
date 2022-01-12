a3ff37f477611a53a389674fe5d59dcc1551fc59
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-01-12 17:10:42 +0200

    Update BudgetCard.js

--- budget-app/src/components/BudgetCard.js
+++ budget-app/src/components/BudgetCard.js
@@ -1,9 +1,15 @@
-import { Card, ProgressBar } from "react-bootstrap";
+import { Button, Card, ProgressBar, Stack } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
-export default function BudgetCard({ name, amount, max }) {
+export default function BudgetCard({ name, max, amount, blue }) {
+  const classNames = [];
+  max < amount
+    ? classNames.push("bg-danger", "bg-opacity-10")
+    : blue
+    ? classNames.push("bg-primary", "bg-opacity-10")
+    : classNames.push("bg-light");
   return (
-    <Card>
+    <Card className={classNames.join(" ")}>
       <Card.Body>
         <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
           <div className="me-2"> {name} </div>
@@ -14,16 +20,25 @@ export default function BudgetCard({ name, amount, max }) {
             </span>
           </div>
         </Card.Title>
-        <ProgressBar className="rounded-pill" variant={getProgressBarVarient(amount, max)} 
-        min={0}
-        max={max}
-        now={amount}/>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVarient(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+        />
+        <Stack direction="horizontal" gap="2" className="mt-4">
+          <Button variant="outline-primary" className="ms-auto">
+            Add Expense
+          </Button>
+          <Button variant="outline-secondary">View Expense</Button>
+        </Stack>
       </Card.Body>
     </Card>
   );
 }
 
 const getProgressBarVarient = (amount, max) => {
- const ratio = amount / max
- return (ratio < 0.5) ? "primary" : (ratio < 0.75) ? "warning" : "danger"
-}
+  const ratio = amount / max;
+  return ratio < 0.5 ? "primary" : ratio < 0.75 ? "warning" : "danger";
+};
